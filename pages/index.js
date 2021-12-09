bdf6a92578c48983fbd37bc07b4c9e9357931003
w.benarto <w.benarto@gmail.com>
2021-12-08 23:29:32 -0800

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main>
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
