749c8722425d105da2db5325ec9d0a7777ced0fa
w.benarto <w.benarto@gmail.com>
2021-12-12 21:10:32 -0800

    error catching

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,14 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
-
+  console.log("middleware" + token);
   const { pathname } = req.nextUrl;
 
   if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
 
-  if (!token && pathname !== "/login") {
-    return NextResponse.redirect("/login");
+  try {
+    if (!token && pathname !== "/login") {
+      return NextResponse.redirect("/login");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
