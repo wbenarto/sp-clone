ac002a2ce2536912fa3f25d72a86267f135dc7df
w.benarto <w.benarto@gmail.com>
2022-01-05 11:27:58 -0800

    middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,22 +5,23 @@ export async function middleware(req) {
   const token = await getToken({
     req,
     secret: process.env.JWT_SECRET,
-    secureCookie:
-      process.env.NEXTAUTH_URL?.startsWith("https://") ??
-      !!process.env.VERCEL_URL,
   });
-  console.log("middleware" + token);
+
+  // Grabs url
   const { pathname } = req.nextUrl;
 
-  if (pathname.includes("/api/auth/callback/spotify") || token) {
+  // if user is already signed in, but goes to login page, redirect to home page
+  if (token && pathname === "/login") {
+    return NextResponse.redirect("/");
+  }
+
+  // if user wants to sign in
+  if (pathname.includes("/api/auth") || token) {
     return NextResponse.next();
   }
 
-  try {
-    if (!token && pathname !== "/login") {
-      return NextResponse.redirect("/login");
-    }
-  } catch (error) {
-    console.log(error);
+  // Redirect to login if there is no token, and are requesting a protected route
+  if (!token && pathname !== "/login") {
+    return NextResponse.redirect("/login");
   }
 }
