c70afcfa0f21c394f22cbcc07eabb9ec0d66ec36
w.benarto <w.benarto@gmail.com>
2022-01-07 10:02:12 -0800

    next url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -5,6 +5,9 @@ export async function middleware(req) {
   const token = await getToken({
     req,
     secret: process.env.JWT_SECRET,
+    secureCookie:
+      process.env.NEXTAUTH_URL?.startsWith("https://") ??
+      !!process.env.VERCEL_URL,
   });
 
   // Grabs url
@@ -15,7 +18,7 @@ export async function middleware(req) {
     return NextResponse.redirect("/");
   }
   // if user wants to sign in
-  if (pathname.includes("/api/auth") || token) {
+  if (pathname.includes("/api/auth/") || token) {
     return NextResponse.next();
   }
 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,6 +37,9 @@ export default NextAuth({
     // ...add more providers here
   ],
   secret: process.env.JWT_SECRET,
+  session: {
+    strategy: "jwt",
+  },
   pages: {
     signIn: "/login",
   },
